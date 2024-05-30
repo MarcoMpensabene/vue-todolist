90619619a929fda280f9bb792bc5322d8676f33d
MarcoMpensabene <marcompensabene@gmail.com>
2024-05-30 16:15:53 +0200

    added functions for invertin value of done , removin and adding object from the list

--- js/script.js
+++ js/script.js
@@ -1,7 +1,48 @@
+const { createApp } = Vue
 createApp({
     data() {
         return {
-        
+        indexList : 0 ,
+        newText : "" ,
+        todoList : [
+            {
+                "text" : "Lavare i vestiti" ,
+                done : false
+            },
+            {
+                "text" : "Pulire la stanza " ,
+                done : false
+            },
+            {
+                "text" : "Fare la spesa" ,
+                done : false
+            },
+            {
+                "text" : "Ripassare Vue" ,
+                done : false
+            },
+            {
+                "text" : "Guardare ultimo episodio di Spice and Wolf" ,
+                done : false
+            },
+        ]
     }
-    }
+    } ,
+    methods: {
+        // ! INVERTIAMO IL VALORE DI done
+        invertDone(indexList) {
+            this.todoList[indexList].done = !this.todoList[indexList].done;
+        },
+        removeText(indexList){
+            this.todoList.splice(indexList , 1);
+        },
+        addNewEl(newText){
+            const newEl = {
+                "text" : newText ,
+                done : false
+            }
+
+            this.todoList.push(newEl)
+        }
+    },
 }).mount('#app')
